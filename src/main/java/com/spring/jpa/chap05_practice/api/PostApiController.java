ba368b7020273d01b1258f3b23d37953453d41f8
dongjun <sdj799@naver.com>
2023-06-20 15:17:23 +0900

    # 10. 종합 연습예제 검증 완료(UPDATE, DELETE)

--- src/main/java/com/spring/jpa/chap05_practice/api/PostApiController.java
+++ src/main/java/com/spring/jpa/chap05_practice/api/PostApiController.java
@@ -1,9 +1,6 @@
 package com.spring.jpa.chap05_practice.api;
 
-import com.spring.jpa.chap05_practice.dto.PageDTO;
-import com.spring.jpa.chap05_practice.dto.PostCreateDTO;
-import com.spring.jpa.chap05_practice.dto.PostDetailResponseDTO;
-import com.spring.jpa.chap05_practice.dto.PostListResponseDTO;
+import com.spring.jpa.chap05_practice.dto.*;
 import com.spring.jpa.chap05_practice.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +10,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -26,7 +24,7 @@ public class PostApiController {
         게시물 목록 조회: /posts       - GET
         게시물 개별 조회: /posts/{id}  - GET
         게시물 등록:     /posts       - POST
-        게시물 수정:     /posts/{id}  - PATCH
+        게시물 수정:     /posts       - PATCH
         게시물 삭제:     /posts/{id}  - DELETE
      */
     private final PostService postService;
@@ -76,6 +74,45 @@ public class PostApiController {
             e.printStackTrace();
             return ResponseEntity.internalServerError()
                     .body("미안 서버 터졌어... 원인: " + e.getMessage());
+        }
+    }
+
+    //게시물 수정
+    @RequestMapping(method = {RequestMethod.PATCH, RequestMethod.PUT})
+    public ResponseEntity<?> update(@Validated @RequestBody PostModifyDTO dto
+                                    , BindingResult result, HttpServletRequest request) {
+
+        log.info("/api/v1/posts {} - dto: {}"
+                    , request.getMethod(), dto);
+        ResponseEntity<List<FieldError>> fieldErrors = getValidatedResult(result);
+        if (fieldErrors != null) return fieldErrors;
+
+        PostDetailResponseDTO responseDTO = postService.modify(dto);
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
+    private static ResponseEntity<List<FieldError>> getValidatedResult(BindingResult result) {
+        if(result.hasErrors()) { //입력값 검증에 걸림
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            fieldErrors.forEach(err -> {
+                log.warn("invalid client data - {}", err.toString());
+            });
+            return ResponseEntity.badRequest().body(fieldErrors);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        log.info("/api/v1/posts/{} DELETE!", id);
+
+        try {
+            postService.delete(id);
+            return ResponseEntity.ok("DEL SUCCESS!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError()
+                    .body(e.getMessage());
         }
     }
 }
